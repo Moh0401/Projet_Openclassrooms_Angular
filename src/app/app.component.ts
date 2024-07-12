95dfccce94bdaf9dac72794b38419cdefb10decf
Moh0401 <161856130+Moh0401@users.noreply.github.com>
2024-07-12 19:35:57 +0000

    2e commit
    19h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FaceSnapListComponent,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
